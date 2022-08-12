d6af23bbfd20b92a3d7c75637781850b12406b3e
Crowdbotics <team@crowdbotics.com>
2022-08-12 04:04:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cordial_cottage_v1_36742",
+  url: "https://cordial_cottage_v1_36742.botics.co",
+  api: "https://cordial_cottage_v1_36742.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
