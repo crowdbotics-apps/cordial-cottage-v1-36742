d3f3557a7a31ba2e403ecb7a39c88e9f74100962
Crowdbotics <team@crowdbotics.com>
2022-08-12 16:10:34 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -13,8 +13,7 @@ export const modulesOptions = {
 
   "@modules/login": {
     LOGO_URL: "https://picsum.photos/200/300",
-    BACKGROUND_URL:
-      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/halfbg.png",
+    BACKGROUND_URL: "https://picsum.photos/200/300",
     HOME_SCREEN_NAME: "Home Screen",
     validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
     SignInNavText: "Sign In",
